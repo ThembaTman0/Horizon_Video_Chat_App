a8374162bfff86b611735ca01054e1314189e609
ThembaTman0 <thembatman0@gmail.com>
2022-08-23 12:48:10 +0200

    v-1.0.2

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
+import { useContext } from 'react'
 import {BsMicMuteFill} from 'react-icons/bs'
+import {SocketContext} from '../SocketContext'
 
 const VideoPlayer = () => {
+  const {name, callAccepted, myVideo, userVideo, callEnded, stream,call} =useContext(SocketContext);
+
   return (
-    <div className='mt-16'>
-      
-      <div class="p-6 w-60 h-60  bg-woodsmoke-50 rounded-lg border border-woodsmoke-50 shadow-md dark:bg-woodsmoke-800 dark:border-gray-700">
-          {/* Name holder */}
+    <div className='mt-16 flex space-x-4'>  
+      {stream &&(
+      <div class="aspect-auto w-96  bg-woodsmoke-50 rounded-lg border border-woodsmoke-50 shadow-md dark:bg-woodsmoke-800 dark:border-gray-700">
+
+          <video playsInline muted ref={myVideo} autoPlay className='rounded-lg w-full h-full'/> 
+          
+      </div>)}
+      {
+        callAccepted && !callEnded && (
+          <div class="p-3 w-60 h-60  bg-woodsmoke-50 rounded-lg border border-woodsmoke-50 shadow-md dark:bg-woodsmoke-800 dark:border-gray-700">
           <div >
             <BsMicMuteFill/>
+            {/* Name holder */}
+            <p className='pt-44'>{call.name || 'Name'}</p>
           </div>
+          <video playsInline muted ref={userVideo} autoPlay/>   
+          
       </div>
+        )
+      }
+
+        
+
 
     </div>
   )
