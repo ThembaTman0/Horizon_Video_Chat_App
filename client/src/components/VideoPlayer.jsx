f9bf6da6b7e48ad500f323759c816c5ac898f658
ThembaTman0 <thembatman0@gmail.com>
2022-08-23 02:01:46 +0200

    v-1.0

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const VideoPlayer = () => {
   return (
-    <div>VideoPlayer</div>
+    <div className='mt-16'>
+      
+      <div class="p-6 w-60 h-60 max-w-sm bg-woodsmoke-50 rounded-lg border border-woodsmoke-50 shadow-md dark:bg-woodsmoke-800 dark:border-gray-700">
+          
+      </div>
+
+    </div>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM  from "react-dom";
 
 import App from './App'
-
+import { ContextProvider } from "./SocketContext";
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<ContextProvider>
+    <App />
+
+</ContextProvider>
+
+, document.getElementById('root'));
