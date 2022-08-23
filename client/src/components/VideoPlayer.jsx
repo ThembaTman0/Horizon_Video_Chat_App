d3877757e69dd8a187316a7b772463b7e1b23c61
ThembaTman0 <thembatman0@gmail.com>
2022-08-23 02:06:56 +0200

    v-1.0.1

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import {BsMicMuteFill} from 'react-icons/bs'
 
 const VideoPlayer = () => {
   return (
     <div className='mt-16'>
       
-      <div class="p-6 w-60 h-60 max-w-sm bg-woodsmoke-50 rounded-lg border border-woodsmoke-50 shadow-md dark:bg-woodsmoke-800 dark:border-gray-700">
-          
+      <div class="p-6 w-60 h-60  bg-woodsmoke-50 rounded-lg border border-woodsmoke-50 shadow-md dark:bg-woodsmoke-800 dark:border-gray-700">
+          {/* Name holder */}
+          <div >
+            <BsMicMuteFill/>
+          </div>
       </div>
 
     </div>
