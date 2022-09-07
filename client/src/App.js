cfd68ec03d82b0363c4e50a7b0b81fe2f1e4de5c
ThembaTman0 <thembatman0@gmail.com>
2022-09-07 23:24:47 +0200

    v-1.3.8

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,10 @@
 import React from 'react'
 import { BrowserRouter,Routes ,Route } from 'react-router-dom'
-import Sidebar from './components/Navagator/Sidebar'
-import Notifications from './components/Notifications'
-import Options from './components/Options'
-import VideoPlayer from './components/VideoPlayer'
+
 import Home from './container/Home/Home'
 import JoinPage from './container/JoinPage/JoinPage'
 import LandingPage from './container/LandingPage/LandingPage'
-      {/* <div className='flex flex-col flex-auto h-screen bg-woodsmoke-900'>
-          <Notifications/>
-          <div className='mx-auto flex justify-center text-center'>
-            
-            <div class="flex justify-center space-x-4 grid-flow-col auto-cols-max">
-              <div><VideoPlayer/>
-              </div>
-            </div>
-            <Options/>
-            
-          </div>
-          
 
-          <Sidebar/>
-      </div> */}
 
 const App = () => {
   return (
